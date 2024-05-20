15b5ae807d7558c6afb15eeb290128a400f71811
M1nX2 <65402758+M1nX2@users.noreply.github.com>
2024-05-20 16:00:28 +0500

    Update urls.py

--- RAG/RAG/urls.py
+++ RAG/RAG/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.apps import RAGapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/',RAGapp.urls),
 ]
